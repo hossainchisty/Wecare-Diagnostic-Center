7e109c0e3c07a8afc3dcdaa0814321aae34a7885
hossainchisty <hossain.chisty11@gmail.com>
2023-02-07 20:51:43 +0600

    ⚰️ Remove dead code

--- report/forms/lab_form.py
+++ report/forms/lab_form.py
@@ -4,14 +4,9 @@ from django.forms import ModelForm
 
 from report.models import Lab
 
-# from django.forms.widgets import DateInput
-
 
 class LabForm(ModelForm):
     ''' Form asking for create patient '''
     class Meta:
         model = Lab
-        fields = ['patient', 'referred_by_doctor', 'report_name', 'report', 'price', 'commission', 'date', 'report_status']
-        # widgets = {
-        #     'date_of_birth': DateInput(attrs={'type': 'date'}),
-        # }
+        fields = ['patient', 'referred_by_doctor', 'report_name', 'report', 'price', 'commission', 'date', 'report_status'] # noqa
