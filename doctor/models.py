16ff3995e235c7c91d275a8458b5c42b04ff4f9e
hossainchisty <hossain.chisty11@gmail.com>
2023-02-11 21:47:01 +0600

    🐛 Added avatar optional

--- doctor/models.py
+++ doctor/models.py
@@ -5,26 +5,23 @@ from django.db import models
 from department.models import Department
 from utility.common_fields import BaseModel
 
-# from utility.unique_uuid import unique_code
-
 
 class Doctor(BaseModel):
     unique_id = models.CharField(max_length=10, null=True, blank=True, editable=False)
     name = models.CharField(max_length=30)
+    avatar = models.ImageField(upload_to='media/', default='static/img/default.jpg', null=True, blank=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     phone = models.CharField(max_length=11)
     designation = models.CharField(max_length=50)
     depertment = models.ForeignKey(Department, on_delete=models.CASCADE)
-    commission = models.CharField(max_length=4, null=True, blank=True, default='0.00')
-    total = models.CharField(max_length=4, null=True, blank=True, default='0.00')
+    commission = models.DecimalField(max_digits=5, decimal_places=2,  default=0.00)
+    total = models.DecimalField(max_digits=5, decimal_places=2,  default=0.00)
     status = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        ''' Encrypted password '''
+        ''' Build encrypted password '''
         self.password = make_password(self.password)
-        ''' unique code generator '''
-        # self.unique_id = f'{unique_code(6)}'
         super(Doctor, self).save(*args, **kwargs)
 
     def __str__(self) -> str:
@@ -33,7 +30,7 @@ class Doctor(BaseModel):
 
 class DoctorVisit(BaseModel):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    visit_charges = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    visit_charges = models.DecimalField(max_digits=5, decimal_places=2,  default=0.00)
     status = models.BooleanField(default=True)
 
     def __str__(self):
