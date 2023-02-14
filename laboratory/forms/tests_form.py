f746d0ba0d5c1193971a0082cfdc49ec5bb614fa
hossainchisty <hossain.chisty11@gmail.com>
2023-02-14 16:40:00 +0600

    ✨ Added tests forms

---
+++ laboratory/forms/tests_form.py
@@ -0,0 +1,12 @@
+# Basic Lib Import
+
+from django.forms import ModelForm
+
+from laboratory.models import Reportlist
+
+
+class TestForm(ModelForm):
+    ''' Form asking for Lab test '''
+    class Meta:
+        model = Reportlist
+        fields = ['title', 'commission', 'price']
