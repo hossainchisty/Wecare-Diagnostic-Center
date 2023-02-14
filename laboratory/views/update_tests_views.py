f6c271b43d3cb1d08e63d3ae3ab6bbc601fa8329
hossainchisty <hossain.chisty11@gmail.com>
2023-02-14 16:46:06 +0600

    ✨ Added lab tests update

---
+++ laboratory/views/update_tests_views.py
@@ -0,0 +1,19 @@
+# Basic Lib Import
+# from django.core.exceptions import PermissionDenied
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
+
+from laboratory.models import Reportlist
+
+
+class UpdateTest(UpdateView):
+    model = Reportlist
+    fields = '__all__'
+    template_name = 'report/tests_form.html'
+    success_url = reverse_lazy('lab_tests_list')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        # if obj.user != self.request.user:
+        #     raise PermissionDenied("You are not allowed to edit this expense.")
+        return obj
