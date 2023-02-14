843ccdae515b4beeed7a7fc0285ff7799a1a9403
hossainchisty <hossain.chisty11@gmail.com>
2023-02-14 22:47:29 +0600

    🐛 Remove year and month field

--- expense/models.py
+++ expense/models.py
@@ -1,5 +1,4 @@
 # Basic Lib Import
-import datetime
 from datetime import date
 
 from django.db import models
@@ -22,15 +21,10 @@ class Category(BaseModel):
 
 class Expense(BaseModel):
     """ Expense model for storing expense data """
-    YEAR_CHOICES = [(y, y) for y in range(2000, datetime.date.today().year+1)]
-    MONTH_CHOICE = [(m, m) for m in range(1, 13)]
-
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     date = models.DateField(default=f'{today}')
     note = models.CharField(max_length=100, null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    year = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year)
-    month = models.IntegerField(choices=MONTH_CHOICE, default=datetime.datetime.now().month)
 
     class Meta:
         verbose_name = 'Expense'
