4961d71f25f9e626504840743f42ec6d45f5558f
hossainchisty <hossain.chisty11@gmail.com>
2023-02-14 16:37:50 +0600

    ✨ Added model into admin

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Doctor, DoctorVisit
+from .models import Doctor, DoctorProfit, DoctorVisit
 
-admin.site.register(Doctor)
+admin.site.register(DoctorProfit)
 admin.site.register(DoctorVisit)
+admin.site.register(Doctor)
