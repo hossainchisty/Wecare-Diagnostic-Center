2f9d9e322c8ed2eefaf53a3d630cee6a7a2d221c
hossainchisty <hossain.chisty11@gmail.com>
2023-02-19 13:42:33 +0600

    ✨ Added urls

--- report/urls.py
+++ report/urls.py
@@ -2,11 +2,14 @@
 from django.urls import path
 
 from report.views.daily_expense_views import DailyExpense
+from report.views.financial_report_views import FinancialReportView
 from report.views.medical_report_views import MedicalReport
 from report.views.monthly_expense_views import MonthlyExpense
 
 # Routing Implement
 urlpatterns = [
+    # Financial Report
+    path('financial/report/', FinancialReportView.as_view(), name='financial_report'),
     # Expense Report
     path('daily/expense/', DailyExpense.as_view(), name='daily_expense'),
     path('monthly/expense/', MonthlyExpense.as_view(), name='monthly_expense'),
