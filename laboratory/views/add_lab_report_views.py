94c2cf821340f8046bfb860cf7c203710e051fe1
hossainchisty <hossain.chisty11@gmail.com>
2023-02-19 13:21:00 +0600

    🚑 Mirror Bug fixed

--- laboratory/views/add_lab_report_views.py
+++ laboratory/views/add_lab_report_views.py
@@ -37,22 +37,22 @@ class CreateLabView(View):
                     total = total + reportPrice.price
                     ''' Get total diagnostic income '''
                     diagnosticProfit = total - totalProfitAmount
-                form.instance.total = total
-                formObject = form.save()
-                ''' Adding diagnostic total profit from tests '''
-                addDiagnosticAmount = DiagnosticIncome.objects.create(
-                    amount=diagnosticProfit,
-                    sources='Earning from lab tests',
-                )
-                addDiagnosticAmount.save()
-                ''' Adding doctor total profit from commission '''
-                addProfit = DoctorProfit.objects.create(
-                            doctor=form.instance.referred_by_doctor,
-                            lab=formObject,
-                            profit=totalProfitAmount
-                            )
-                addProfit.save()
-                """Provide a redirect on GET request."""
-                return redirect('lab_report_list')
-            else:
-                return render(request,  'report/add_lab_report.html', {'form': LabForm()})
+            form.instance.total = total
+            formObject = form.save()
+            ''' Adding diagnostic total profit from tests '''
+            addDiagnosticAmount = DiagnosticIncome.objects.create(
+                amount=diagnosticProfit,
+                sources='Earning from lab tests',
+            )
+            addDiagnosticAmount.save()
+            ''' Adding doctor total profit from commission '''
+            addProfit = DoctorProfit.objects.create(
+                        doctor=form.instance.referred_by_doctor,
+                        lab=formObject,
+                        profit=totalProfitAmount
+                        )
+            addProfit.save()
+            """Provide a redirect on GET request."""
+            return redirect('lab_report_list')
+        else:
+            return render(request,  'report/add_lab_report.html', {'form': LabForm()})
