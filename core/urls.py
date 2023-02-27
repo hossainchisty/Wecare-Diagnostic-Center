6625a8e074d08bb6c0e9980078bbe184f386b42f
hossainchisty <hossain.chisty11@gmail.com>
2023-02-28 02:22:56 +0600

    🎨 Update version

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,8 @@
 from django.urls import path
 
 from core.views.core_views import Dashboard
-from core.views.notifications import ExpiredMedicine, StockOutReport
 
 # Routing Implement
 urlpatterns = [
     path('', Dashboard.as_view(), name='dashboard'),
-    path('notifications/expired/medicine/', ExpiredMedicine.as_view(), name='expired_medicine'),
-    path('notifications/stock/out/medicine/', StockOutReport.as_view(), name='stock_out_medicine'),
 ]
